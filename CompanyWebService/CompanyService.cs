b625edd74a93508eae04d975f8759b0eed2f2ad7
Kodpencerem <emrullah04@outlook.com>
2023-05-31 10:58:49 +0300

    Add web service host

--- CompanyWebService/CompanyService.cs
+++ CompanyWebService/CompanyService.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
-
-namespace CompanyWebService
+﻿namespace CompanyWebService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CompanyService" in both code and config file together.
-    public class CompanyService : ICompanyService
+    public class CompanyService : ICompanyPublicService, ICompanyConfidentialService
     {
-        public void DoWork()
+       
+        public string GetConfidentialInformation()
         {
+            return "This Is Confidential Information, TCP";
+        }
+
+        public string GetPublicInformation()
+        {
+            return "This Is Public Information, HTTP";
         }
     }
 }
--- CompanyWebService/ICompanyService.cs
+++ CompanyWebService/ICompanyService.cs
@@ -4,9 +4,16 @@ namespace CompanyWebService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICompanyService" in both code and config file together.
     [ServiceContract]
-    public interface ICompanyService
+    public interface ICompanyPublicService
     {
         [OperationContract]
-        void DoWork();
+        string GetPublicInformation();
+    }
+
+    [ServiceContract]
+    public interface ICompanyConfidentialService
+    {
+        [OperationContract]
+        string GetConfidentialInformation();
     }
 }
