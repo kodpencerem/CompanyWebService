9f8cf830ee917e1ecf48543b44a955f3447e39cf
Kodpencerem <emrullah04@outlook.com>
2023-05-31 11:35:22 +0300

    Configuring services

--- CompanyWebServiceHost/Program.cs
+++ CompanyWebServiceHost/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace CompanyWebServiceHost
+﻿using CompanyWebService;
+using System;
+using System.ServiceModel;
+
+namespace CompanyWebServiceHost
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            using (ServiceHost host = new ServiceHost(typeof(CompanyWebService.CompanyService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @" + DateTime.Now);
+                Console.ReadLine();
+            }
         }
     }
 }
